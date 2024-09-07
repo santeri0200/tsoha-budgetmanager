3e588396fbe46891b17104577675e9fb1cbd2341
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2024-09-07 21:28:21 +0300

    Added database capability

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from os import getenv
+
 # Webserver
 from flask import Flask
 
@@ -11,12 +13,15 @@ from flask import redirect, url_for
 from flask import render_template
 
 APPNAME="Budget manager"
-app = Flask(__name__, static_folder='static', static_url_path='/static')
-app.secret_key = b'MYSECRETKEY' # TODO: Add this to .env
+app = Flask(__name__, static_folder="static", static_url_path='/static')
+app.secret_key = getenv("SECRET_KEY")
 
-users = {
-    "test": "test"
-}
+# Database
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+from werkzeug.security import check_password_hash, generate_password_hash
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
 
 def nav_items(path: str):
     items = [
@@ -29,14 +34,29 @@ def nav_items(path: str):
 
     return items
 
+@app.route("/api/test")
+def api():
+    sql = text("SELECT * FROM users WHERE name = :username")
+    res = db.session.execute(sql, {"username": "test"}).fetchone()
+    if not res:
+        sql = text("INSERT INTO users VALUES (DEFAULT, :username, :password)")
+        print(f"{ len(generate_password_hash("test")) }")
+        db.session.execute(sql, {"username": "test", "password": generate_password_hash("test")})
+        db.session.commit()
+
+    return redirect(url_for('index'))
+
 @app.route("/login", methods=["POST"])
 def login():
     username = request.form["username"]
     password = request.form["password"]
 
-    if username in users and users[username] == password:
+    sql = text("SELECT password FROM users WHERE name = :username")
+    res = db.session.execute(sql, {"username": username}).fetchone()
+    print(res)
+    if res and check_password_hash(res[0], password):
         session["username"] = username
-
+        
     return redirect(url_for('index'))
 
 @app.route("/logout")
